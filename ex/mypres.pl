b435aae62cd4f134a414b35105d3b771c0ad1d7c
Joel Berger <joel.a.berger@gmail.com>
2013-11-01 21:33:31 -0500

    some little fixes

--- ex/mypres.pl
+++ ex/mypres.pl
@@ -1,11 +1,9 @@
-use Mojo::Base -strict;
-
 my (undef, $dir) = app->presentation_file;
 
 my $conf = {
   slides => {
     list => ['hello', 'goodbye'],
-    bootstrap_theme => 1,
   },
+  bootstrap_theme => 1,
   templates => $dir,
 };
